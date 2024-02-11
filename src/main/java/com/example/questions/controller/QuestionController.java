f2ebc788c700fe2da96b96fa8faf46872c9706b0
SerbanTiurbe21 <serban.tiurbe@student.upt.ro>
2024-02-11 19:43:00 +0200

    - updated the endpoints route - added dependencies for micrometer and zipkin

--- src/main/java/com/example/questions/controller/QuestionController.java
+++ src/main/java/com/example/questions/controller/QuestionController.java
@@ -13,7 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/questions")
+@RequestMapping("/api/v1/questions")
 @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class QuestionController {
 
--- src/main/java/com/example/questions/controller/TopicController.java
+++ src/main/java/com/example/questions/controller/TopicController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/topics")
+@RequestMapping("/api/v1/topics")
 public class TopicController {
 
     private final TopicServiceImpl topicServiceImpl;
